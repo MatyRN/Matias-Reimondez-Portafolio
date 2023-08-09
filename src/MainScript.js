a3102c57353eeccc37a7ff1162b3494e92494609
MatRN <51181381+MatyRN@users.noreply.github.com>
2023-08-09 09:17:59 -0300

    Add files via upload

---
+++ src/MainScript.js
@@ -0,0 +1,10 @@
+// Get the video
+var video = document.getElementById("VideoBackground");
+
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    }
+}
